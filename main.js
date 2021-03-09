fc62919d13e108f16c2a3a0d1ab4c6a95851607e
joshua <joshua@gmail.com>
2021-03-09 22:27:19 +0800

    adding style to the p

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    
+    document.querySelector('p').addEventListener('click',set)
+
+    function set(){
+        console.log(this.textContent)
+    }
 })
